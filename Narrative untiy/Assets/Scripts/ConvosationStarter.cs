2b74b224d5b66fbe907dfd13a43c78b3cca8a666
Bragh2001 <112693932+Bragh2001@users.noreply.github.com>
2025-11-13 15:09:12 +0100

    Start working on dialogue boxes

---
+++ Narrative untiy/Assets/Scripts/ConvosationStarter.cs
@@ -0,0 +1,22 @@
+using DialogueEditor;
+using UnityEngine;
+
+public class Convo : MonoBehaviour
+{
+
+    [SerializeField] private NPCConversation myConversation;
+
+    private void OnTriggerStay(Collider other)
+    { 
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                ConversationManager.Instance.StartConversation(myConversation);
+            }
+        }
+    
+    }
+
+
+}
