5fe8de0fdec4a2dc80a38d330f3c875dd919232a
Goodest person <mflens22@student.aau.dk>
2025-11-19 11:21:07 +0100

    Player movement og Camera follow

---
+++ Narrative untiy/Assets/Scripts/Camcontrol.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Camcontrol : MonoBehaviour
+{
+    public Transform Player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Camera controls
+        float targetX = Player.position.x;
+        float targetY = Player.position.y + 2f;
+        
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+    }
+}
